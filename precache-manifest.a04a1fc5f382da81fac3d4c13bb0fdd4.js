d9404e9397f284052c886e300bbcbb63db36bd1f
Mateusz Prejs <mat.prejs@googlemail.com>
2020-09-04 22:34:46 +0200

    Updates

---
+++ precache-manifest.a04a1fc5f382da81fac3d4c13bb0fdd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc76e3b65e09538b78c5ddaac80f690",
+    "url": "/Eware/index.html"
+  },
+  {
+    "revision": "1609666b60df86efda43",
+    "url": "/Eware/static/css/main.8a240ba7.chunk.css"
+  },
+  {
+    "revision": "ddb402bff580bd730559",
+    "url": "/Eware/static/js/2.9bf7d4e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Eware/static/js/2.9bf7d4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1609666b60df86efda43",
+    "url": "/Eware/static/js/main.6de287f0.chunk.js"
+  },
+  {
+    "revision": "cfff912c8739df713830",
+    "url": "/Eware/static/js/runtime-main.ad57a8db.js"
+  }
+]);
